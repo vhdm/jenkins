b3a2f9f36a321892bada0f1dc45761e373e3196e
Vahid <vhdm@users.noreply.github.com>
2020-11-28 00:32:32 +0330

    Update init.py

--- init.py
+++ init.py
@@ -1,3 +1,3 @@
 import os
-os.makedir("/opt/test-jenkins",exist_ok=True)
-print("Folder Created At '/opt/test-jenkins'")
+os.mkdir("/opt/test-jenkins",exist_ok=True)
+print("Folder Created Successfully in '/opt/test-jenkins'")
